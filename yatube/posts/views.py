4140fe771bed7edcf7bd34af3c38cc213d07b9b4
Demian <kislinskiidg@yandex.ru>
2021-09-25 01:25:33 +0200

    Project is ready. Code check and submit for review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from django.http import HttpResponse
-from .models import Post
+from .models import Post, Group
 
 # Главная страница
 def index(request):
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
     # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
+        'title': title,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context) 
@@ -19,11 +19,13 @@ def index(request):
 
 # Страница с постами сгруппированными по темам
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Записи сообщества'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group__slug=slug).order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': title,
-        'text': text,
+        'slug': group.slug,
+        'title': group.title,
+        'description': group.description,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
